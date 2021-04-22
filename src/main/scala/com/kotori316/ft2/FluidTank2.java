df749a1170f7d4cf6e7ddcc280fd40ccad2f925e
Kotori316 <Kotori316@gmail.com>
2021-04-22 11:45:14 +0900

    Some changes to make mods

--- src/main/scala/com/kotori316/ft2/FluidTank2.java
+++ src/main/scala/com/kotori316/ft2/FluidTank2.java
@@ -1,4 +1,12 @@
 package com.kotori316.ft2;
 
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(FluidTank2.modID)
 public class FluidTank2 {
+    public static final String modID = "fluidtank";
+
+    public FluidTank2() {
+    }
+
 }

c857071f3312042eaf5c04402c998b097c3f999b
Kotori316 <Kotori316@gmail.com>
2023-08-13 22:33:25 +0900

    Use show to convert GenericAmount to String

--- common/src/main/scala/com/kotori316/fluidtank/contents/FluidTransferLog.scala
+++ common/src/main/scala/com/kotori316/fluidtank/contents/FluidTransferLog.scala
@@ -1,6 +1,7 @@
 package com.kotori316.fluidtank.contents
 
 import cats.Show
+import cats.implicits.showInterpolator
 
 sealed trait FluidTransferLog {
   def logString: String
@@ -11,23 +12,23 @@ sealed trait FluidTransferLog {
 object FluidTransferLog {
 
   case class FillFluid[A](toFill: GenericAmount[A], filled: GenericAmount[A], before: Tank[A], after: Tank[A]) extends FluidTransferLog {
-    override def logString: String = s"FillFluid{Filled=$filled, ToFill=$toFill, Before={${before.content}}, After={${after.content}}}"
+    override def logString: String = show"FillFluid[Filled=$filled, ToFill=$toFill, Before=${before.content}, After=${after.content}]"
   }
 
   case class FillFailed[A](fluid: GenericAmount[A], tank: Tank[A]) extends FluidTransferLog {
-    override def logString: String = s"FillFailed{FailedToFill=$fluid, Tank={${tank.content}}}"
+    override def logString: String = show"FillFailed[FailedToFill=$fluid, Tank=${tank.content}]"
   }
 
   case class FillAll[A](fluid: GenericAmount[A], tank: Tank[A]) extends FluidTransferLog {
-    override def logString: String = s"FillAll{Filled=$fluid, Tank={${tank.content}}}"
+    override def logString: String = show"FillAll[Filled=$fluid, Tank=${tank.content}]"
   }
 
   case class DrainFluid[A](toDrain: GenericAmount[A], drained: GenericAmount[A], before: Tank[A], after: Tank[A]) extends FluidTransferLog {
-    override def logString: String = s"DrainFluid{Drained=$drained, ToDrain=$toDrain, Before={${before.content}}, After={${after.content}}}"
+    override def logString: String = show"DrainFluid[Drained=$drained, ToDrain=$toDrain, Before=${before.content}, After=${after.content}]"
   }
 
   case class DrainFailed[A](fluid: GenericAmount[A], tank: Tank[A]) extends FluidTransferLog {
-    override def logString: String = s"DrainFailed{ToDrain=$fluid, Tank={${tank.content}}}"
+    override def logString: String = show"DrainFailed[ToDrain=$fluid, Tank=${tank.content}]"
   }
 
   implicit val showFluidTransferLog: Show[FluidTransferLog] = _.logString

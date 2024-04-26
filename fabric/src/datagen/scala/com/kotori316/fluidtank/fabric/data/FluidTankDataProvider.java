095026b58585db077eb4743e518eb18a8da9430d
Kotori316 <Kotori316@gmail.com>
2024-04-27 00:33:51 +0900

    Loot table

--- fabric/src/datagen/scala/com/kotori316/fluidtank/fabric/data/FluidTankDataProvider.java
+++ fabric/src/datagen/scala/com/kotori316/fluidtank/fabric/data/FluidTankDataProvider.java
@@ -3,6 +3,11 @@ package com.kotori316.fluidtank.fabric.data;
 import com.kotori316.fluidtank.FluidTankCommon;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
+
+import java.util.List;
+import java.util.Set;
 
 public final class FluidTankDataProvider implements DataGeneratorEntrypoint {
     @Override
@@ -11,5 +16,11 @@ public final class FluidTankDataProvider implements DataGeneratorEntrypoint {
         var pack = fabricDataGenerator.createPack();
 
         pack.addProvider(RecipeProvider::new);
+        pack.addProvider((o, future) -> new LootTableProvider(
+            o,
+            Set.of(),
+            List.of(new LootTableProvider.SubProviderEntry(() -> new LootSubProvider(o, future), LootContextParamSets.BLOCK)),
+            future)
+        );
     }
 }

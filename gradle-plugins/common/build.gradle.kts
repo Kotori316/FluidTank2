0e9d954116cea3ee5ecc393f701da1df0d49eb9c
Kotori316 <Kotori316@gmail.com>
2024-05-09 18:22:00 +0900

    Change plugin location

--- gradle-plugins/common/build.gradle.kts
+++ gradle-plugins/common/build.gradle.kts
@@ -14,10 +14,11 @@ dependencies {
     mapOf(
         "architectury-plugin" to libs.versions.plugin.architectury.get(),
         "dev.architectury.loom" to libs.versions.plugin.loom.get(),
-        "com.kotori316.plugin.cf" to libs.versions.plugin.cf.get(),
+        // "com.kotori316.plugin.cf" to libs.versions.plugin.cf.get(),
         "com.github.johnrengelman.shadow" to libs.versions.plugin.shadow.get(),
         "me.modmuss50.mod-publish-plugin" to libs.versions.plugin.publish.all.get(),
     ).forEach { (name, version) ->
         implementation(group = name, name = "${name}.gradle.plugin", version = version)
     }
+    implementation(group = "com.kotori316", name = "call-plugin", version = libs.versions.plugin.cf.get())
 }

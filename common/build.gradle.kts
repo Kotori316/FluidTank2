73b1846fa3cdfe66520344f6b631d5aefecd2c2c
Kotori316 <Kotori316@gmail.com>
2024-07-09 23:29:24 +0900

    Forge compile

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -27,5 +27,5 @@ dependencies {
     })
     // We depend on fabric loader here to use the fabric @Environment annotations and get the mixin dependencies
     // Do NOT use other classes from fabric loader
-    modImplementation("net.fabricmc:fabric-loader:${project.property("fabric_loader_version")}")
+    modLocalRuntime("net.fabricmc:fabric-loader:${project.property("fabric_loader_version")}")
 }

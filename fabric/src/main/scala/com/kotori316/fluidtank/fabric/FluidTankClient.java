81ed09f29f1111f51153991de0cacac5e37a177c
Kotori316 <Kotori316@gmail.com>
2024-12-14 23:04:00 +0900

    Register reservoir special model in Forge and Fabric

--- fabric/src/main/scala/com/kotori316/fluidtank/fabric/FluidTankClient.java
+++ fabric/src/main/scala/com/kotori316/fluidtank/fabric/FluidTankClient.java
@@ -2,13 +2,17 @@ package com.kotori316.fluidtank.fabric;
 
 import com.kotori316.fluidtank.FluidTankCommon;
 import com.kotori316.fluidtank.fabric.message.PacketHandler;
+import com.kotori316.fluidtank.fabric.render.FluidRenderHelperFabric;
 import com.kotori316.fluidtank.fabric.render.RenderTankFabric;
+import com.kotori316.fluidtank.render.RenderItemCodecs;
 import com.kotori316.fluidtank.render.ReservoirModel;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.SpecialBlockRendererRegistry;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
+import net.minecraft.client.renderer.special.SpecialModelRenderers;
 
 public final class FluidTankClient implements ClientModInitializer {
     @Override
@@ -26,6 +30,7 @@ public final class FluidTankClient implements ClientModInitializer {
         // var reservoirRenderer = new RenderReservoirItemFabric();
         EntityModelLayerRegistry.registerModelLayer(ReservoirModel.LOCATION, ReservoirModel::createDefinition);
         // FluidTank.RESERVOIR_MAP.values().forEach(b -> BuiltinItemRendererRegistry.INSTANCE.register(b, reservoirRenderer));
+        SpecialModelRenderers.ID_MAPPER.put(RenderItemCodecs.RESERVOIR_MODEL, RenderItemCodecs.reservoirModelUnbaked(new FluidRenderHelperFabric()).type());
 
         BlockEntityRenderers.register(FluidTank.TILE_TANK_TYPE, RenderTankFabric::new);
         BlockEntityRenderers.register(FluidTank.TILE_CREATIVE_TANK_TYPE, RenderTankFabric::new);

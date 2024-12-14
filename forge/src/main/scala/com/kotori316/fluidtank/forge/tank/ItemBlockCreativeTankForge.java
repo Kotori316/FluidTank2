ce2b2cdcddccde279084d3ffe14977ad0ec3aee8
Kotori316 <Kotori316@gmail.com>
2024-12-14 14:24:35 +0900

    compile main

--- forge/src/main/scala/com/kotori316/fluidtank/forge/tank/ItemBlockCreativeTankForge.java
+++ forge/src/main/scala/com/kotori316/fluidtank/forge/tank/ItemBlockCreativeTankForge.java
@@ -1,26 +1,10 @@
 package com.kotori316.fluidtank.forge.tank;
 
-import com.kotori316.fluidtank.forge.render.RenderItemTank;
 import com.kotori316.fluidtank.tank.BlockTank;
 import com.kotori316.fluidtank.tank.ItemBlockCreativeTank;
-import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
-import net.minecraftforge.client.extensions.common.IClientItemExtensions;
-
-import java.util.function.Consumer;
 
 public final class ItemBlockCreativeTankForge extends ItemBlockCreativeTank {
     public ItemBlockCreativeTankForge(BlockTank b) {
         super(b);
-    }
-
-    @Override
-    public void initializeClient(Consumer<IClientItemExtensions> consumer) {
-        super.initializeClient(consumer);
-        consumer.accept(new IClientItemExtensions() {
-            @Override
-            public BlockEntityWithoutLevelRenderer getCustomRenderer() {
-                return RenderItemTank.INSTANCE();
-            }
-        });
     }
 }
--- forge/src/main/scala/com/kotori316/fluidtank/forge/tank/ItemBlockVoidTankForge.java
+++ forge/src/main/scala/com/kotori316/fluidtank/forge/tank/ItemBlockVoidTankForge.java
@@ -1,26 +1,10 @@
 package com.kotori316.fluidtank.forge.tank;
 
-import com.kotori316.fluidtank.forge.render.RenderItemTank;
 import com.kotori316.fluidtank.tank.BlockTank;
 import com.kotori316.fluidtank.tank.ItemBlockVoidTank;
-import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
-import net.minecraftforge.client.extensions.common.IClientItemExtensions;
-
-import java.util.function.Consumer;
 
 public final class ItemBlockVoidTankForge extends ItemBlockVoidTank {
     public ItemBlockVoidTankForge(BlockTank b) {
         super(b);
-    }
-
-    @Override
-    public void initializeClient(Consumer<IClientItemExtensions> consumer) {
-        super.initializeClient(consumer);
-        consumer.accept(new IClientItemExtensions() {
-            @Override
-            public BlockEntityWithoutLevelRenderer getCustomRenderer() {
-                return RenderItemTank.INSTANCE();
-            }
-        });
     }
 }

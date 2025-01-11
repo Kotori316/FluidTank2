4aa3860804611f66d096ac20023e4df179c847ca
Kotori316 <Kotori316@gmail.com>
2025-01-11 14:14:43 +0900

    Take a picture for tank

--- fabric/src/clientTest/scala/com/kotori316/fluidtank/fabric/client/test/DemoTest.java
+++ fabric/src/clientTest/scala/com/kotori316/fluidtank/fabric/client/test/DemoTest.java
@@ -4,24 +4,40 @@ import com.kotori316.fluidtank.tank.PlatformTankAccess;
 import com.kotori316.fluidtank.tank.Tier;
 import net.fabricmc.fabric.api.client.gametest.v1.ClientGameTestContext;
 import net.fabricmc.fabric.api.client.gametest.v1.FabricClientGameTest;
-import net.minecraft.core.BlockPos;
+import net.minecraft.client.gui.screens.worldselection.WorldCreationUiState;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 @SuppressWarnings("UnstableApiUsage")
 public final class DemoTest implements FabricClientGameTest {
     @Override
     public void runTest(ClientGameTestContext context) {
-        try (var singlePlayerContext = context.worldBuilder().create()) {
+        try (var singlePlayerContext = context.worldBuilder().adjustSettings(DemoTest::setWorldNameByTime).create()) {
             singlePlayerContext.getServer().runOnServer(server -> {
                 var level = server.getLevel(Level.OVERWORLD);
-                level.setBlock(new BlockPos(5, -60, 6), PlatformTankAccess.getInstance().getTankBlockMap().get(Tier.WOOD).get().defaultBlockState(), Block.UPDATE_ALL);
-                for (var player : level.players()) {
-                    player.moveTo(3, -60, 6, 0, 0);
-                }
+                var player = server.getPlayerList().getPlayers().getFirst();
+                var direction = player.getDirection();
+                var pos = player.getOnPos().above();
+                level.setBlock(pos.relative(direction, 3), PlatformTankAccess.getInstance().getTankBlockMap().get(Tier.WOOD).get().defaultBlockState(), Block.UPDATE_ALL);
             });
             singlePlayerContext.getClientWorld().waitForChunksRender();
             context.takeScreenshot("demo");
         }
+    }
+
+    static void setWorldNameByTime(WorldCreationUiState state) {
+        var name = createWorldNameByDate(ZonedDateTime.now());
+        setWorldName(state, name);
+    }
+
+    static void setWorldName(WorldCreationUiState state, String name) {
+        state.setName(name);
+    }
+
+    static String createWorldNameByDate(ZonedDateTime now) {
+        return now.format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss"));
     }
 }

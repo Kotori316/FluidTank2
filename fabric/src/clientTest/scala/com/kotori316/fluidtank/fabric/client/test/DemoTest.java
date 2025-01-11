bb87f9a36eaa4b9e986dcc6c46f1126e48e248e2
Kotori316 <Kotori316@gmail.com>
2025-01-11 14:50:46 +0900

    Add a test for water tank

--- fabric/src/clientTest/scala/com/kotori316/fluidtank/fabric/client/test/DemoTest.java
+++ fabric/src/clientTest/scala/com/kotori316/fluidtank/fabric/client/test/DemoTest.java
@@ -4,18 +4,14 @@ import com.kotori316.fluidtank.tank.PlatformTankAccess;
 import com.kotori316.fluidtank.tank.Tier;
 import net.fabricmc.fabric.api.client.gametest.v1.ClientGameTestContext;
 import net.fabricmc.fabric.api.client.gametest.v1.FabricClientGameTest;
-import net.minecraft.client.gui.screens.worldselection.WorldCreationUiState;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 
 @SuppressWarnings("UnstableApiUsage")
 public final class DemoTest implements FabricClientGameTest {
     @Override
     public void runTest(ClientGameTestContext context) {
-        try (var singlePlayerContext = context.worldBuilder().adjustSettings(DemoTest::setWorldNameByTime).create()) {
+        try (var singlePlayerContext = context.worldBuilder().adjustSettings(FluidTankClientGameTest::setWorldNameByTime).create()) {
             singlePlayerContext.getServer().runOnServer(server -> {
                 var level = server.getLevel(Level.OVERWORLD);
                 var player = server.getPlayerList().getPlayers().getFirst();
@@ -26,18 +22,5 @@ public final class DemoTest implements FabricClientGameTest {
             singlePlayerContext.getClientWorld().waitForChunksRender();
             context.takeScreenshot("demo");
         }
-    }
-
-    static void setWorldNameByTime(WorldCreationUiState state) {
-        var name = createWorldNameByDate(ZonedDateTime.now());
-        setWorldName(state, name);
-    }
-
-    static void setWorldName(WorldCreationUiState state, String name) {
-        state.setName(name);
-    }
-
-    static String createWorldNameByDate(ZonedDateTime now) {
-        return now.format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss"));
     }
 }

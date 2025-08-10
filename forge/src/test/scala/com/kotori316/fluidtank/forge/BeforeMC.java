b1dc3a3376e6417c0d867f5ca24576bd495f17a9
Kotori316 <Kotori316@gmail.com>
2025-08-10 15:19:06 +0900

    Remove dep to test_utility

--- forge/src/test/scala/com/kotori316/fluidtank/forge/BeforeMC.java
+++ forge/src/test/scala/com/kotori316/fluidtank/forge/BeforeMC.java
@@ -3,7 +3,6 @@ package com.kotori316.fluidtank.forge;
 import com.kotori316.fluidtank.FluidTankCommon;
 import com.kotori316.fluidtank.config.ConfigData;
 import com.kotori316.fluidtank.config.PlatformConfigAccess;
-import com.kotori316.testutil.MCTestInitializer;
 import org.junit.jupiter.api.BeforeAll;
 
 public abstract class BeforeMC {

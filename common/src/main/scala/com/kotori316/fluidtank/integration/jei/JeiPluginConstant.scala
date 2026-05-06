70b014ecdadab8943a2b239e283a665b22a08420
Kotori316 <Kotori316@gmail.com>
2026-05-06 11:32:49 +0900

    Create JEI plugin to remove shapeless icon from JEI

---
+++ common/src/main/scala/com/kotori316/fluidtank/integration/jei/JeiPluginConstant.scala
@@ -0,0 +1,8 @@
+package com.kotori316.fluidtank.integration.jei
+
+import com.kotori316.fluidtank.FluidTankCommon
+import net.minecraft.resources.Identifier
+
+object JeiPluginConstant {
+  val pluginUid: Identifier = Identifier.fromNamespaceAndPath(FluidTankCommon.modId, "jei_plugin")
+}
